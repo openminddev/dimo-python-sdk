9f54be16bf0d3ee0cf629c27087ee9fc0214902c
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-07-27 09:59:18 -0600

    FIX: Identity mistakenly expected access token; fix synatx for list_vehicle_definitions_per_address query; readme revision

--- dimo/identity.py
+++ dimo/identity.py
@@ -3,11 +3,11 @@ class Identity:
         self.dimo = dimo_instance
 
     # Primary query method
-    async def query(self, token, query):
-        return await self.dimo.query('Identity', query, token=token)
+    async def query(self, query):
+        return await self.dimo.query('Identity', query)
 
     # Sample query - count DIMO vehicles
-    async def count_dimo_vehicles(self, token):
+    async def count_dimo_vehicles(self):
         query = """
         {
             vehicles (first:10) {
@@ -15,29 +15,34 @@ class Identity:
             }
         }
         """
-        return await self.dimo.query('Identity', query, token=token)
+        return await self.dimo.query('Identity', query)
 
     # Sample query - list vehicle definitions per address
-    async def list_vehicle_definitions_per_address(self, address, limit, token):
-        query = f"""
-                {{
-                    vehicles(filterBy: owner: {address}, first: {limit}) {{
-                      nodes {{
-                        aftermarketDevice {{
-                            tokenId
-                            address
-                        }}
-                          syntheticDevice {{
-                            address
-                            tokenId
-                        }}
-                        definition {{
-                          make
-                          model
-                          year
-                        }}
-                      }}
-                    }}
-                  }}
-                """
-        return await self.dimo.query('Identity', query, token=token)
+    async def list_vehicle_definitions_per_address(self, address, limit):
+        query = """
+        query ListVehicleDefinitionsPerAddress($owner: Address!, $first: Int!) {
+          vehicles(filterBy: {owner: $owner}, first: $first) {
+            nodes {
+              aftermarketDevice {
+                tokenId
+                address
+              } 
+              syntheticDevice {
+                address
+                tokenId
+              }
+              definition {
+                make
+                model
+                year
+              }
+            }
+          }
+        }
+        """
+        variables = {
+            "owner": address,
+            "first": limit
+        }
+
+        return await self.dimo.query('Identity', query, variables=variables)
